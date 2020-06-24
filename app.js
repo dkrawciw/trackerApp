9f9c4ae7c06be4615b31e8da2e9ab7c502500680
Daniel Krawciw <46691574+dkrawciw@users.noreply.github.com>
2020-06-23 20:17:36 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express'),
+      app = express(),
+      bodyParser = require('body-parser'),
+      mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host    : '127.0.0.1',
+  user    : 'root',
+  password: 'xD#X3227l!7K&LW4k4av',
+  database: 'interrobang_db',
+  port    : '3306'
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get('/', function(req, res){
+  connection.query('SELECT id, time_checked, DATEDIFF(NOW(), time_checked) AS time_passed FROM checked_times', function(err, results, fields){
+    if(err) throw err;
+    res.render('index.ejs', {results: results});
+  });
+});
+app.post('/deleteTime/:time', function(req, res){
+  connection.query('DELETE FROM checked_times WHERE id = ' + req.params.time + ';' );
+  res.redirect('/');
+});
